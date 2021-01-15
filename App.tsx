b56df180ee5dce15bf223d51ae692c6d346e5c1a
Fayiz-A <68380206+Fayiz-A@users.noreply.github.com>
2021-01-15 17:59:18 +0530

    added bottom tab navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageSourcePropType, Dimensions } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import WriteStoryScreen from './screens/writeStoryScreen';
+import ReadStoryScreen from './screens/readStoryScreen';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Tab = createBottomTabNavigator();
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={
+            ({route})=>({
+              tabBarIcon: ({ focused, color, size}) => {
+                let imagePath:ImageSourcePropType;
+
+                if(route.name == "Read Story") {
+                  imagePath = require("./read.png");
+                } else {
+                  imagePath = require("./write.png");
+                } 
+
+                let largeScreen:boolean = Dimensions.get("window").width>700;
+                let imageSize:number = largeScreen?40:30;
+
+                return <Image source={imagePath} style={{width: imageSize, height: imageSize,}}/>
+              }
+            })
+          }
+          tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+            // tabStyle: {...{flexDirection: "column"}},
+          }}
+        >
+          <Tab.Screen name="Write Story" component={WriteStoryScreen} />
+          <Tab.Screen name="Read Story" component={ReadStoryScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ components/AppBar.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export interface Props {
+  title: String,
+}
+
+class AppBar extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.textStyle}>{this.props.title}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white"
+  },
+  container: {
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    elevation: 2,    
+    alignItems: "center",
+    justifyContent: "center",
+    height: 60,
+    backgroundColor: "black"
+  },
+});
+
+export default AppBar;
